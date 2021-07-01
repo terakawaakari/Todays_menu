61e171dff2ef5f068a5e82336436aea97ef3be88
terakawaakari <terakawaakari0701@gmail.com>
2021-07-01 14:32:23 +0000

    マイページ枠組み作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :users, only: [:show, :update]
+  get 'users/withdraw_confirm' => 'users#withdraw_confirm', as: 'withdraw_confirm'
+  patch 'users/withdraw' => 'users#withdraw', as: 'withdraw'
+
 end
