0d4cf43ec670f5bd71261cced85ecc72a75be812
terakawaakari <terakawaakari0701@gmail.com>
2021-07-09 15:24:28 +0000

    買い物リストの非同期化

--- app/controllers/buy_items_controller.rb
+++ app/controllers/buy_items_controller.rb
@@ -6,34 +6,37 @@ class BuyItemsController < ApplicationController
   end
 
   def create
+    @items = current_user.buy_items
     @item = current_user.buy_items.new(buy_item_params)
-    if @item.save && request.referrer.include?("recipes")
+    if request.referrer.include?("recipes")
+      @item.save
       redirect_to request.referrer, notice: "買い物リストに「#{@item.name}」を追加しました"
-    elsif  @item.save
-      redirect_to buy_items_path
+    else
+      @item.save
+      @item = current_user.buy_items.new
     end
   end
 
   def update
+    @items = current_user.buy_items
     item = BuyItem.find(params[:id])
     item.update(buy_item_params)
-    redirect_to buy_items_path
   end
 
   def destroy
+    @items = current_user.buy_items
     item = current_user.buy_items.find(params[:id])
     item.destroy
-    redirect_to buy_items_path
   end
 
   def all_destroy
+    @items = current_user.buy_items
     current_user.buy_items.destroy_all
-    redirect_to buy_items_path
   end
 
   def bought_destroy
+    @items = current_user.buy_items
     current_user.buy_items.where(is_bought: true).destroy_all
-    redirect_to buy_items_path
   end
 
   private
