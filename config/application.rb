3c6065c74a2230504e3e0f79034fc8c5b1d5b8cf
terakawaakari <terakawaakari0701@gmail.com>
2021-07-12 07:18:11 +0000

    データがない場合の表示作成

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module TodaysMenu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,3 +19,4 @@ module TodaysMenu
     # the framework and any gems in your application.
   end
 end
+
