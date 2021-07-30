3b809b97f5f831134d9cf2e2dd2cac15c1d5dbd4
terakawaakari <terakawaakari0701@gmail.com>
2021-07-30 05:18:12 +0000

    ルーレットの記述修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -140,25 +140,21 @@ $(document).on('turbolinks:load', function(){
   var start_button = $("#start");
   var stop_button  = $("#stop");
   var reset_button = $("#reset");
-  var roulette;
 
   reset_button.css('display', 'none');
+  stop_button.prop("disabled", true);
   // スタートボタンを押した時の処理
   start_button.on('click', function () {
     $(this).prop("disabled", true);
-    roulette = setInterval(function(){
-      circle.addClass('rotate');
-    });
+    stop_button.prop("disabled", false);
+    circle.addClass('rotate');
   });
   // ストップボタンを押した時の処理
   stop_button.on('click',function () {
-    if(roulette) {
-      clearInterval(roulette);
-      circle.addClass('stop');
-      stop_button.blur();
-      reset_button.css('display', 'inline-block');
-      start_button.css('display', 'none');
-    };
+    circle.addClass('stop');
+    stop_button.blur();
+    reset_button.css('display', 'inline-block');
+    start_button.css('display', 'none');
   });
   // リセットボタンを押した時の処理
   reset_button.on('click',function () {
@@ -167,6 +163,7 @@ $(document).on('turbolinks:load', function(){
     reset_button.css('display', 'none');
     start_button.css('display', 'inline-block');
     start_button.prop("disabled", false);
+    stop_button.prop("disabled", true);
   });
 });
 
